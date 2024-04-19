c7b35eb1b399faa45f78189c6cdecd0327937059
nurbekomar <omarnurbek13@gmail.com>
2024-04-19 08:44:53 +0500

    update setting.py

--- nurbektech/nurbektech/settings.py
+++ nurbektech/nurbektech/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-$ptto^by1v54c**)l3g2-c@yhd8gd&e--d-a*!k*#)fk5hma7l"
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", cast=bool)
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -80,8 +81,12 @@ WSGI_APPLICATION = "nurbektech.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "dbblog",
+        "USER": "nurbek",
+        "PASSWORD": "1234",
+        "HOST": "localhost",
+        "PORT": "",
     }
 }
 
@@ -139,6 +144,7 @@ CKEDITOR_JQUERY_URL = "//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.j
 CKEDITOR_CONFIGS = {
     "default": {
         "toolbar": "full",
+        "width": "100%",
         "extraPlugins": ",".join(
             [
                 "codesnippet",
