73b776a86cdb9bf676ee485af386fa89755574ac
nurbekomar <omarnurbek13@gmail.com>
2024-05-09 21:02:11 +0500

    update

--- nurbektech/nurbektech/settings.py
+++ nurbektech/nurbektech/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", cast=bool)
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "nurbektech.kz", "86.107.44.98", "www.nurbektech.kz"]
 
 
 # Application definition
