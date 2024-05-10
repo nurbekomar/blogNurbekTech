bd5bf7ce7cb1b6c08f858904434807793c3c2723
nurbekomar <omarnurbek13@gmail.com>
2024-05-10 15:59:46 +0500

    update settings

--- nurbektech/nurbektech/urls.py
+++ nurbektech/nurbektech/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
@@ -26,7 +26,8 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("blog.urls")),
     path("ckeditor/", include("ckeditor_uploader.urls")),
-    path(r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}),
+    re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+    re_path(r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
